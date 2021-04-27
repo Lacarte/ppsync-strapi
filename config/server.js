2de734dbb9a2d5a24babf243dd8300ea80375c1a
mr.Lacarte <carteasy@gmail.com>
2021-04-27 06:22:13 -0500

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2fd4067fa56f2908fe65205b84eb524d'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'f0461053-2f87-41e9-8b20-10073e756572'
+};
