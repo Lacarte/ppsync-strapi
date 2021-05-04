147cf40db2fc603c15b5e4b203396e939338620e
mr.Lacarte <carteasy@gmail.com>
2021-05-04 07:03:40 -0500

    test ctx request in doctype,

--- api/doctype/controllers/doctype.js
+++ api/doctype/controllers/doctype.js
@@ -5,4 +5,28 @@
  * to customize this controller
  */
 
-module.exports = {};
+const { sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+
+
+
+  async find(ctx) {
+    let entities;
+    if (ctx.query._q) {
+      entities = await strapi.services.doctype.search(ctx.query);
+    } else {
+      entities = await strapi.services.doctype.find(ctx.query);
+    }
+
+    console.log('ctx.query._q >',ctx.query._q);
+    console.log('ctx >',ctx);
+    console.log('entities >',entities);
+
+    return entities.map(entity => sanitizeEntity(entity, { model: strapi.models.doctype }));
+  },
+
+
+
+
+};
--- extensions/users-permissions/config/jwt.js
+++ extensions/users-permissions/config/jwt.js
@@ -1,3 +1,3 @@
 module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '7d4460d4-3949-4415-83c5-d71435eb73ed'
+  jwtSecret: process.env.JWT_SECRET || '7d4460d4-3949-4415-83c5-d71435eb73ed',
 };
